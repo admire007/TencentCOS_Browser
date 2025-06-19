d12804a0e2d477d73ad24c2f3898846144a10a3e
admire007 <153353373+admire007@users.noreply.github.com>
2025-06-19 14:31:18 +0800

    Delete Stock+Login+DBasic_Final_5 directory

--- Stock+Login+DBasic_Final_5/qos/qosbrowser/msvc_make.bat
+++
@@ -1,2 +0,0 @@
-chcp 65001
-"E:\visual studio\VC\Tools\MSVC\14.29.30133\bin\HostX64\x64\nmake.exe" %*
--- Stock+Login+DBasic_Final_5/qos/qosbrowser/src/fend/uicom/uitableview.cpp
+++
@@ -1,7 +0,0 @@
-﻿#include "uitableview.h"
-#include <src/fend/uidelegates/uitableitemdelegate.h>
-
-UiTableView::UiTableView(QWidget *parent):QTableView(parent)
-{
-    setItemDelegate(new UiTableItemDelegate(this));
-}
